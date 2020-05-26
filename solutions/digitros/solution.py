68222a485dbb797a448e4f257aeea3f0ed40a867
Diego Valdez <dgogo9@hotmail.com>
2020-05-26 17:29:45 -0500

    Corrección para snake_case

--- solutions/digitros/solution.py
+++ solutions/digitros/solution.py
@@ -4,20 +4,20 @@ from typing import List
 class Solution:
 
     def duplicate_zeros(self, arr: List[int]):
-        zeroAcum = 0
+        zero_acum = 0
         last = 0
-        arrayLen = len(arr) - 1 # 0
+        array_len = len(arr) - 1 # 0
 
         for idx, value in enumerate(arr):
-            if value == 0 and idx + zeroAcum < arrayLen:
-                zeroAcum += 1 #0
+            if value == 0 and idx + zero_acum < array_len:
+                zero_acum += 1 #0
                 last = idx #4
 
         for _ in arr:
-            if arrayLen is not 0:
-                arr[arrayLen] = arr[arrayLen - zeroAcum]
-                if arr[arrayLen] == 0 and zeroAcum > 0 and arrayLen - zeroAcum <= last:
-                    arr[arrayLen - 1] = 0
-                    zeroAcum -= 1
-                    arrayLen -= 1
-                arrayLen -= 1
+            if array_len is not 0:
+                arr[array_len] = arr[array_len - zero_acum]
+                if arr[array_len] == 0 and zero_acum > 0 and array_len - zero_acum <= last:
+                    arr[array_len - 1] = 0
+                    zero_acum -= 1
+                    array_len -= 1
+                array_len -= 1
